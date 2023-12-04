e66b42c45708e30a74b31e34b8b66c6e1621d6d5
Hogan Zhang <george.zhz123@gmail.com>
2023-12-04 20:19:01 +0800

    Add files via upload

---
+++ backend/src/handler/httpServer.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("src/static/index.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
